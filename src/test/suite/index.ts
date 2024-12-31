6aedca8324f5f1c94ba500104f9f25b4afb087af
Tobias Raabe <raabe@posteo.de>
2024-12-31 14:27:26 +0100

    Install code formatter and format.

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -3,33 +3,32 @@ import * as Mocha from 'mocha';
 import * as fs from 'fs';
 
 export function run(): Promise<void> {
-    // Create the mocha test
-    const mocha = new Mocha({
-        ui: 'tdd',
-        color: true
-    });
+  // Create the mocha test
+  const mocha = new Mocha({
+    ui: 'tdd',
+    color: true,
+  });
 
-    const testsRoot = path.resolve(__dirname, '.');
+  const testsRoot = path.resolve(__dirname, '.');
 
-    return new Promise((resolve, reject) => {
-        try {
-            // Get all test files
-            const files = fs.readdirSync(testsRoot)
-                .filter(file => file.endsWith('.test.js'));
+  return new Promise((resolve, reject) => {
+    try {
+      // Get all test files
+      const files = fs.readdirSync(testsRoot).filter((file) => file.endsWith('.test.js'));
 
-            // Add files to the test suite
-            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+      // Add files to the test suite
+      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
-            // Run the mocha test
-            mocha.run(failures => {
-                if (failures > 0) {
-                    reject(new Error(`${failures} tests failed.`));
-                } else {
-                    resolve();
-                }
-            });
-        } catch (err) {
-            reject(err);
+      // Run the mocha test
+      mocha.run((failures) => {
+        if (failures > 0) {
+          reject(new Error(`${failures} tests failed.`));
+        } else {
+          resolve();
         }
-    });
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
 }
